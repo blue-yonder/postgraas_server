0701316e4944c649b6cd9074b1f57de2cdb948ec
Sebastian Neubauer <sebastian.neubauer@blue-yonder.com>
2017-01-08 00:38:49 +0100

    bugfix in db init script

--- postgraas_server/management_database.py
+++ postgraas_server/management_database.py
@@ -61,6 +61,6 @@ def init_db(db_credentials, postgraas_app):
 
     from postgraas_server.management_resources import db
     with postgraas_app.app_context():
-        if not is_sane_database(db.Model, db.get_engine()):
+        if not is_sane_database(db.Model, db.session):
             db.drop_all()
             db.create_all()
