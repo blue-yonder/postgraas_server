452299c5dc19c26e6b32b72466069657d5833c98
Sebastian Neubauer <sebastian.neubauer@blue-yonder.com>
2017-09-19 10:15:30 +0200

    added a test for username normalisation

--- tests/test_unit/backends/postgres_cluster/test_postgres_cluster_driver.py
+++ tests/test_unit/backends/postgres_cluster/test_postgres_cluster_driver.py
@@ -11,3 +11,6 @@ def test_get_normalized_username():
     username_with_host = 'testuser1234@testcluster1'
     stripped_username = pgcd.get_normalized_username(username_with_host)
     assert stripped_username == "testuser1234"
+    username_without_host = 'testuser1234'
+    stripped_username = pgcd.get_normalized_username(username_with_host)
+    assert stripped_username == "testuser1234"
